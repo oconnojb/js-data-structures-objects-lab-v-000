3dd553467dfe87ec2a22ca09192c2e9f41cdb152
John O'Connor <oconnojb@gmail.com>
2018-06-07 21:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const driver = {};
 
 function updateDriverWithKeyAndValue(obj, key, val) {
   let newdriver = Object.assign({}, obj, {key, val});
-  console.log("hello")
+  console.log(newdriver)
   return newdriver;
 }
