3e571cd02a0ed15867fb630d28279974fb4058ff
John O'Connor <oconnojb@gmail.com>
2018-06-07 21:59:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function updateDriverWithKeyAndValue(obj, key, val) {
   let newObj = {}
   newObj[key] = val
   let newdriver = Object.assign({}, obj, newObj);
-  console.log(newdriver)
   return newdriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, val) {
+  obj.key = val
 }
